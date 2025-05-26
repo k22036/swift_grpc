2a20d13d9d26b4ccf1f26841ad7df70a995010a9
k22036 <131113333+k22036@users.noreply.github.com>
2025-05-26 09:26:37 +0900

    feat: gRPC client using swift

---
+++ client/grpc_client_ios/grpc_client_ios/ContentView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  grpc_client_ios
+//
+//  Created by k22036kk on 2025/05/22.
+//
+
+import SwiftUI
+import GRPCCore
+import GRPCNIOTransportHTTP2
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+        .onAppear() {
+            Task {
+                print("Connecting to gRPC server...")
+                do {
+                    try await withGRPCClient(
+                        transport: .http2NIOPosix(
+                            target: .dns(host: "localhost", port: 5300),
+                            transportSecurity: .plaintext
+                        )
+                    ) { client in
+                        print("Connected to gRPC server.")
+                        let pinger = Pinger_Pinger.Client(wrapping: client)
+                        let pong = try await pinger.ping(request: .init(message: Pinger_Empty()))
+                        print(pong)
+                    }
+                } catch {
+                    print("Failed to connect to gRPC server: \(error)")
+                }
+            }
+        }
+    }
+}
+
+//#Preview {
+//    ContentView()
+//}
---
+++ client/grpc_client_ios/grpc_client_ios/grpc_client_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  grpc_client_iosApp.swift
+//  grpc_client_ios
+//
+//  Created by k22036kk on 2025/05/22.
+//
+
+import SwiftUI
+
+@main
+struct grpc_client_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
